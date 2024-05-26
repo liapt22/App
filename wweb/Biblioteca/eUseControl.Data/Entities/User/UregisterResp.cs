ef7ba89219e8c0cbbccd74c6c0c59c2f47a5c432
Liliana Petrachi <petrachililiana4@gmail.com>
2024-05-27 00:16:56 +0300

    i've changed something

---
+++ wweb/Biblioteca/eUseControl.Data/Entities/User/UregisterResp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUseControl.Data.Entities.User
+{
+    public class UregisterResp
+    {
+        public bool Status { get; set; }
+        public string StatusMsg { get; set; }
+    }
+}
